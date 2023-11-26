d3dc36617738f7aeeb15c5786fb1700399c256ec
Jason Tai <alchenerd@gmail.com>
2023-11-27 02:39:10 +0800

    Load decklist in a modal at index/ and post the selected decklist to play/

--- play/consumers.py
+++ play/consumers.py
@@ -3,6 +3,25 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import datetime
 
+class PlayConsumer(WebsocketConsumer):
+    def connect(self):
+        async_to_sync(self.channel_layer.group_add)(
+                'user',
+                self.channel_name
+        )
+        self.accept()
+        self.send(text_data=json.dumps({'log': 'hello world from consumer'}))
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+                'user',
+                self.channel_name
+        )
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        type_ = text_data_json['type']
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.deck_name = self.scope['url_route']['kwargs']['deck_name']
