7011555fc4138261ea9ddade729bf16d46cc4d6a
Jason Tai <alchenerd@gmail.com>
2023-12-15 00:54:28 +0800

    Implement decklist dialog

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,15 +7,70 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActionArea from '@mui/material/CardActionArea';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
 import './index.css';
 
 function TopFiveMetaDecks() {
+  const [open, setOpen] = useState(false);
+  const [readonly, setReadonly] = useState(false);
+  const [targetDeckName, setTargetDeckName] = useState('');
+  const [targetDecklist, setTargetDecklist] = useState('');
   const [results, setResults] = useState([]);
   const customDeck = {
     "name": "Custom Deck",
     "meta": "???",
     "decklist": "",
     "art": "https://cards.scryfall.io/large/front/b/d/bd07b50a-0bd7-49fa-9ada-96a8f96a62cc.jpg",
+  };
+
+  const handleClickOpen = (deck) => {
+    setTargetDeckName(deck.name);
+    if (deck.name === 'Custom Deck') {
+      setReadonly(false);
+    } else {
+      setReadonly(true);
+    }
+    setTargetDecklist(deck.decklist.trim());
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      decklist: targetDecklist,
+    };
+
+    try {
+      const response = await axios.post('http:/localhost:8000/api/play', data);
+      setOpen(false);
+      window.location.href = 'play/';
+    } catch (error) {
+      if (error.response) {
+        const statusCode = error.response.status;
+        const errorMessage = error.response.data.message;
+        switch (statusCode) {
+          case 400:
+            console.error('Server-side validation errors:', errorMessage);
+          default:
+            console.error('HTTP error:', statusCode, errorMessage);
+        }
+      } else if (error.request) {
+        console.error('Network error:', error.request);
+      } else {
+        console.error('Error:', error.message);
+      }
+    }
   };
 
   useEffect(() => {
@@ -34,7 +89,7 @@ function TopFiveMetaDecks() {
         {results.map(deck => (
           <Grid item key={deck.name} xs={12} md={4}>
           <Card sx={{ minWidth: 350 }}>
-            <CardActionArea>
+            <CardActionArea onClick={() => handleClickOpen(deck)}>
               <CardMedia
                 style={{ backgroundSize: '118%', backgroundPosition: '50% 26%', margin: 'auto' }}
                 sx={{ height: 150 }}
@@ -54,6 +109,30 @@ function TopFiveMetaDecks() {
           </Grid>
         ))}
       </Grid>
+      <Dialog open={open} onClose={handleClose} scroll='paper'>
+        <DialogTitle>
+          Challenge Ned Decker with {targetDeckName}
+        </DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+          <TextField
+            autoFocus
+            id="decklist"
+            label="Decklist"
+            fullWidth
+            defaultValue={targetDecklist}
+            variant="standard"
+            multiline
+            InputProps={{
+              readOnly: readonly,
+            }}
+            helperText='Example: "4 Colossal Dreadmaw"'
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSubmit}>Play</Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };
